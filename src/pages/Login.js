1c862585d68adbcf0590bcdff919554c2cec080f
Adebola Oyenuga <debolacodes@gmail.com>
2023-10-19 12:26:31 +0100

    feat: authentication

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,12 +2,18 @@
 
 import React, { useState, useContext } from 'react';
 import LoginWidget from './components/LoginWigdet';
-
+import { useNavigate } from 'react-router-dom';
 export default function SignUp() {
-
+    const navigate = useNavigate()
     return (
         <div className="admin-page">
             <LoginWidget />
+            <div 
+            className='login_shortnote'
+            onClick={()=>{
+                navigate('/signup')
+            }}
+            ><p>Don't have an account, Register</p></div>
         </div>
     );
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,13 +2,19 @@
 
 import React, { useState, useContext } from 'react';
 import SignUpWidget from './components/SignUpWidget'
-
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
-
+    const navigate = useNavigate()
     return (
         <div className="admin-page">
             <SignUpWidget />
+            <div 
+            className='login_shortnote'
+            onClick={()=>{
+                navigate('/login')
+            }}
+            ><p>Already have an account, Click here to login</p></div>
         </div>
     );
 }
